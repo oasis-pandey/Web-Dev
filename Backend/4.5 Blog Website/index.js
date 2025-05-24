f1a2e511df2c32aace5e49918802bc57713b4d3c
Oasis Pandey <oasispandey2024@gmail.com>
2025-05-24 14:32:21 -0500

    JSON parsing and stringify

--- Backend/4.5 Blog Website/index.js
+++ Backend/4.5 Blog Website/index.js
@@ -1,3 +1,14 @@
 import express from "express"
 
 const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+
+
+
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}.`)
+})
