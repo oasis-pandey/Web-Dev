e930405f2fd25a083b2f839848ec68aea876a722
Oasis Pandey <oasispandey2024@gmail.com>
2025-05-24 16:03:41 -0500

    create logic add

--- Backend/4.5 Blog Website/index.js
+++ Backend/4.5 Blog Website/index.js
@@ -1,10 +1,35 @@
 import express from "express"
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+let blogsTitle = [];
+let blogsContent=[];
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs")
+    res.render("index.ejs",{
+        Title : blogsTitle,
+        Content : blogsContent
+    })
+})
+
+app.post("/create", (req,res) => {
+    res.render("create.ejs")
+})
+
+app.post("/delete",(req,res)=>{
+    if(req.body.action == "delete"){
+        
+    }
+})
+
+app.post("/post",(req,res)=>{
+    blogsTitle.push(req.body.title)
+    blogsContent.push(req.body.content)
+    res.redirect("/")
 })
 
 
