5245010b5a59e6399a7d67a8f80db73bc5963de7
Oasis Pandey <oasispandey2024@gmail.com>
2025-06-20 22:24:33 -0500

    capstone project update

--- Backend/5.7 API Capstone/server.js
+++ Backend/5.7 API Capstone/server.js
@@ -4,24 +4,40 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let prompt;
 
 app.use(bodyParser.urlencoded({extended:true}))
+const GEMINI_API_KEY = "AIzaSyC5oehxth5wlqlfcPj3taHkphxNiZljBew";
 
 app.get("/", async (req,res)=>{
-    const response = await axios.get("https://icanhazdadjoke.com/",{
+    const response = await axios.post(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+        {
+  "contents": [
+    {
+      "parts": [
+        {
+          "text": `I am going to tell you the name of a dish and you will need to give me with all the necessary ingredients in a bullet point. Just list the most top 5 common ingredients along with a short recipe afterwards. Here is the food: ${prompt}`
+        }
+      ]
+    }
+  ]
+},
+        {
         headers : {
-            "Accept" : "application/json"
+            "Content-Type" : "application/json"
         }
     })
-    const result = JSON.stringify(response.data.joke);
-    res.render("index.ejs",{
-        joke : result
-    })
+    let result = JSON.stringify(response.data.candidates[0].content.parts[0].text);
+    result = result.replace(/\n/g , '<br>')
+    console.log(result);
+    res.render("index.ejs",{response : result})
 })
 
-app.post("/next",(req,res)=>{
-    res.redirect("/");
+app.post("/submit",(req,res)=>{
+    prompt = req.body.type[0];
+    res.redirect("/")
 })
+
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}.`)
