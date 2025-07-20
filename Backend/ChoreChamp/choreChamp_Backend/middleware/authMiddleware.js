ead0d7e470752d15b8ff34f85099626eb17ba708
Oasis Pandey <oasispandey2024@gmail.com>
2025-07-19 23:58:35 -0500

    organized code

--- Backend/ChoreChamp/choreChamp_Backend/middleware/authMiddleware.js
+++ Backend/ChoreChamp/choreChamp_Backend/middleware/authMiddleware.js
@@ -6,7 +6,13 @@ const protect = async (req, res, next) => {
     try{
         if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
         const token = req.headers.authorization.slice(7)
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        // decodes the token and gives a js object, 
+        // {
+        // id: '650ab123cd456ef789...',
+        // iat: 1721390000,
+        // exp: 1721994800
+        // }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) 
         const user = await User.findById(decoded.id).select("-password")
         req.user = user;
         next()
